4fb2ebb4fc18eaf1d51cf549af84028431c84595
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2025-01-02 11:17:47 +0530

    Add files via upload

---
+++ finally.java
@@ -0,0 +1,25 @@
+
+
+public class Test7 {
+ 
+	public static void main(String[] args) {
+
+		try {
+
+			String name=null;
+
+			System.out.println(name.length());
+
+		}
+
+		finally {
+
+			System.out.println("Hello ,Hi");
+
+	}
+ 
+}
+
+}
+
+ 
